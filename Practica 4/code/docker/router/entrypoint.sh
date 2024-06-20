f814c0defac775fb2ba29fa486019d5605ee807c
JaviConce <javi.conce.2000@gmail.com>
2024-06-20 20:32:28 +0200

    Correcion

--- Practica 4/code/docker/router/entrypoint.sh
+++ Practica 4/code/docker/router/entrypoint.sh
@@ -23,19 +23,19 @@ iptables -t nat -A POSTROUTING -o eth0 -p icmp -j MASQUERADE
 
 #Permitir protocolo TCP y UDP
 iptables -A INPUT -p udp --sport 53 -j ACCEPT
-iptables -A INPUT -p ucp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
 iptables -A INPUT -p tcp --sport 53 -j ACCEPT
-iptables -A INPUT -p tdp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
 
 iptables -A FORWARD -p udp --sport 53 -j ACCEPT
-iptables -A FORWARD -p ucp --dport 53 -j ACCEPT
+iptables -A FORWARD -p udp --dport 53 -j ACCEPT
 iptables -A FORWARD -p tcp --sport 53 -j ACCEPT
-iptables -A FORWARD -p tdp --dport 53 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 53 -j ACCEPT
 
 iptables -t nat -A POSTROUTING -o eth0 -p udp --dport 53 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o eth0 -p ucp --sport 53 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -p udp --sport 53 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -p tcp --dport 53 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o eth0 -p tdp --sport 53 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -p tcp --sport 53 -j MASQUERADE
 
 #Aceptamos TCP en el puerto 443
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
@@ -54,7 +54,7 @@ iptables -A FORWARD -i eth3 -o eth1 -j ACCEPT
 iptables -A FORWARD -i eth2 -o eth2 -j ACCEPT
 
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 5000 -j DNAT --to-destination 10.0.1.4:5000
-iptables -t nat -A POSTROUTING -o eth1 -p tcp --dport 5000 -s 172.17.0.0/16 -d 10.0.1.4 -j SNAT --to-source 10.0.1.2
+iptables -t nat -A POSTROUTING -o eth1 -p tcp --dport 5000 -s 127.0.0.0/16 -d 10.0.1.4 -j SNAT --to-source 10.0.1.2
 
 #Reenvio de la interfaz de entrada y salida por TCP en el puerto 5000
 iptables -A FORWARD -i eth0 -o eth1 -p tcp --syn --dport 5000 -m state --state NEW -j ACCEPT
@@ -71,7 +71,7 @@ iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCE
 iptables -A FORWARD -i eth1 -o eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 22 -j DNAT --to-destination 10.0.1.3
-iptables -t nat -A POSTROUTING -o eth1 -p tcp --dport 22 -s 172.17.0.0/16 -d 10.0.1.3 -j SNAT --to-source 10.0.1.2
+iptables -t nat -A POSTROUTING -o eth1 -p tcp --dport 22 -s 127.0.0.0/16 -d 10.0.1.3 -j SNAT --to-source 10.0.1.2
 
 
 
